d294ee8fd6a07b0931a87156ad35c8b9f534a07a
sssghulamrasool <seositesoft7@gmail.com>
2023-07-21 16:14:52 +0500

    changes

--- src/pages/add-toolkit/index.jsx
+++ src/pages/add-toolkit/index.jsx
@@ -134,12 +134,12 @@ const AddToolkit = () => {
             <h2 className="sale-view-title">Event Viewer</h2>
           </div>
         </div>
-        <div className="row align-items-center">
-          <div className="col-lg-5 col-md-5 col-6">
+        <div className="row align-items-center justify-content-center">
+          <div className="col-lg-11 col-md-5 col-6">
             <div className="sale-view-search-input position-relative">
               <input
                 type="text"
-                placeholder="Seach by viagogo link"
+                placeholder="search event"
                 value={searchResult}
                 onChange={hanldeChangeInputResult}
               />
@@ -149,7 +149,7 @@ const AddToolkit = () => {
             </div>
           </div>
 
-          <div className="col-lg-1 col-md-1 col-6">
+          <div className="col-lg-1  col-md-1 col-6">
             <Link
               to={`/serach?search-result=${searchResult}`}
               className="search-button text-decoration-none"
@@ -157,23 +157,6 @@ const AddToolkit = () => {
               {" "}
               Search
             </Link>
-          </div>
-          <div className="col-lg-6 col-md-7 col-12">
-            <div className="filter-by-wrapper d-flex justify-content-between align-items-center">
-              <h1 className="filter-by"> Filter by: </h1>
-              <select name="" id="" className="form-select">
-                <option value="">row </option>
-                <option value="">row 1</option>
-                <option value="">row 2 </option>
-              </select>
-              <select name="" id="" className="form-select">
-                <option value="">seat type </option>
-                <option value="">seat type 1</option>
-                <option value="">seat type 2</option>
-              </select>
-
-              <input type="date" className="form-control" />
-            </div>
           </div>
         </div>
       </div>
@@ -239,7 +222,7 @@ const AddToolkit = () => {
           </div>
         </div>
       </div>
-      <div className="container-xxl">
+      {/* <div className="container-xxl">
         <div className="row graphic">
           <div className="col-md-6">
             <div className="d-flex justify-content-between align-items-end mb-5">
@@ -337,7 +320,7 @@ const AddToolkit = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* {opedPopUp && (
         <React.Fragment>
