89b3de0be7b8208ac3ba5657eec7b34fe3edc2df
Rehman Ali <softdix@gmail.com>
2023-07-24 20:48:58 +0500

    udpated code

--- src/pages/add-toolkit/index.jsx
+++ src/pages/add-toolkit/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import masjid from "../../assets/images/faisal-masjid.jpg";
 import { Link, useNavigate } from "react-router-dom";
 import {
